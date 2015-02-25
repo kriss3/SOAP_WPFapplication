a331d114d8074fa75c8e3d0569245bb0f72e8f6f
kriss3 <kris_s2@outlook.com>
2015-02-25 15:47:53 -0800

    First commit to SOAP service + WPF client app

---
+++ MyWebService/MyService.asmx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MyWebService
+{
+	/// <summary>
+	/// This SOAP Service will have 5 WebMethods to be exposed via proxy
+	/// different clients (C#, ASP.Net page, Java) should be able to "tap" into those methods
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/", 
+		Description = "A Simple Web Calculator Service",
+		Name = "CalculatorWebService")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+	// [System.Web.Script.Services.ScriptService]
+	public class MyService : System.Web.Services.WebService
+	{
+		[WebMethod]
+		public int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+		[WebMethod]
+		public int Subtract(int x, int y)
+		{
+			return x - y;
+		}
+
+		[WebMethod]
+		public int Multiply(int x, int y)
+		{
+			return x * y;
+		}
+
+		[WebMethod]
+		public double Divide(double x, double y)
+		{
+			if (y == 0)
+			{
+				throw new ArithmeticException("Please check the argument passed to the method, the value is: " + y);
+			}
+			else
+			{
+				return x / y;
+			}
+		}
+
+		[WebMethod]
+		public string GetMyString(string messageValue)
+		{
+			return messageValue.ToUpperInvariant();
+		}
+	}
+}
---
+++ WebServiceClient/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebServiceClient
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			txtBox1.Text = string.Empty;
+			txtBox2.Text = string.Empty;
+			
+		}
+
+		private string GetTxtFromRichTextBox(RichTextBox rbx)
+		{
+			var val = new TextRange(rbx	.Document.ContentStart, rbx.Document.ContentEnd);
+			return val.Text;
+		}
+
+		private string GetResults(string val1, string val2, int function)
+		{
+			MyServiceReference.CalculatorWebServiceSoapClient client = new MyServiceReference.CalculatorWebServiceSoapClient();
+
+			int a, b;
+			string result = string.Empty; ;
+
+
+			if (!int.TryParse(val1, out a))
+			{
+				lblResults.Content = "Something wrong with the first argument";
+				return string.Empty;
+			}
+
+
+			if (!int.TryParse(val2, out b))
+			{
+				lblResults.Content = "Something wrong with the second argument";
+				return string.Empty;
+			}
+
+			try
+			{
+				switch (function)
+				{
+					case 0:
+						result = val1 + " + " + val2 + " = " + client.Add(a, b);
+						break;
+					case 1:
+						result = val1 + " - " + val2 + " = " + client.Subtract(a, b);
+						break;
+					case 2:
+						result = val1 + " * " + val2 + " = " + client.Multiply(a, b);
+						break;
+					case 3:
+						result = val1 + " / " + val2 + " = " + client.Divide(a, b);
+						break;
+					case 4:
+						result = client.GetMyString(GetTxtFromRichTextBox(rTxtBoxOne));
+						break;
+					default:
+						result = "Wrong function picked !!!";
+						break;
+				}
+
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("Cannot divide by 0\n" + e.Message);
+				throw;
+			}
+
+			return result;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (txtBox1.Text != string.Empty && txtBox2.Text != string.Empty)
+			{
+				lblResults.Content = GetResults(txtBox1.Text, txtBox2.Text, cbFunctions.SelectedIndex);
+			}
+		}
+
+	}
+}
